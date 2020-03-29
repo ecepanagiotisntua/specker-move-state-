3101f42e1c2b719cbc97b6e69e23897d045d9e28
ecepanagiotisntua <61820984+ecepanagiotisntua@users.noreply.github.com>
2020-03-29 19:34:31 +0300

    Update state.cpp

--- state.cpp
+++ state.cpp
@@ -20,21 +20,47 @@ friend ostream & operator << (ostream &out, const State &state);
 };
 
 State::State(int h, const int c[]){
-heaps = h;
-coins = new int[heaps];
-  for (int i = 0; i < heaps; i++)
-    coins[i] = c[i];
+  heaps = h;
+  coins = new int[heaps];
+    for (int i = 0; i < heaps; i++)
+      coins[i] = c[i];
 }
 
 State::~State(){
-delete [] coins;
-return;
+  delete [] coins;
+  return;
 }
 
 void State::next(const Move &move) throw (logic_error){
-if (move.getSource() < 0 || move.getSource() > heaps) throw logic_error("invalid heap");
-if (move.getTarget() < 0 || move.getTarget() > heaps) throw logic_error("invalid heap");
-if (move.getSourceCoins() > coins[get.Source()] || move.getSourceCoins() < 0 || move.getTargetCoins > coins[move.getTarget()] || move.getTargetCoins() < 0) throw logic_error ("invalid heap");
-coins[move.getSource()] -= move.getSourceCoins();
-coins[move.getTarget()] += move.getTargetCoins();
+  if (move.getSource() < 0 || move.getSource() >= heaps) throw logic_error("invalid heap");
+  if (move.getTarget() < 0 || move.getTarget() >= heaps) throw logic_error("invalid heap");
+  if (move.getSourceCoins() > coins[get.Source()] || move.getSourceCoins() < 0 || move.getTargetCoins > coins[move.getTarget()] || move.getTargetCoins() < 0) throw logic_error ("invalid heap");
+  coins[move.getSource()] -= move.getSourceCoins();
+  coins[move.getTarget()] += move.getTargetCoins();
+}
+
+bool State::winning() const{
+  bool flag = true;
+  for (int i = 0; i < heaps; i++){
+    if (coins[i] != 0) flag = false;
+    break;
+  }
+  return flag;
+}
+
+int State::getHeaps() const{
+  return heaps;
+}
+
+int State::getCoins(int h) const throw(logic_error){
+  if (h < 0 || h >= heaps) throw logic_error("invalid heap");
+  return coins[h];
+}
+
+ostream & operator << (ostream &out, const State &state){
+  out << coins[0];
+  for (int i = 1; i < heaps; i++){
+    out << ", " << getCoins(i);
+  }
+  return out;
 }
