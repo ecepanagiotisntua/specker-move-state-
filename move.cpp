676bafc671e5eb92d2b5b349351ed1feb83b5c42
ecepanagiotisntua <61820984+ecepanagiotisntua@users.noreply.github.com>
2020-03-29 19:37:54 +0300

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -16,3 +16,35 @@ int getTargetCoins() const;
 
 friend ostream & operator << (ostream &out, const Move &move);
 };
+
+Move::Move(int sh, int sc, int th, int tc){
+sourceh = sh;
+sourcec = sc;
+targeth = th;
+targetc = tc;
+}
+
+int Move::getSource() const {
+return sourceh;
+}
+
+int Move::getSourceCoins() const {
+return sourcec;
+}
+
+int Move::getTarget() const {
+return targeth;
+}
+
+int Move::getTargetCoins() const {
+return targetc;
+}
+
+ostream& operator << (ostream &out, const Move &move){
+if (getTargetCoins() > 0)
+out << "takes " << getSourceCoins() << " coins from heap " << getSource() << " and puts " << getTargetCoins() << " coins to heap " << getTarget();
+else
+out << "takes " << getSourceCoins() << " coins from heap " << getSource() << " and puts nothing";
+return out;
+}
+
