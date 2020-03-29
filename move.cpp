7197c7299c3e1f7a94e841c456472e14c3a123ff
ecepanagiotisntua <61820984+ecepanagiotisntua@users.noreply.github.com>
2020-03-29 20:05:26 +0300

    Update move.cpp

--- move.cpp
+++ move.cpp
@@ -1,50 +1,55 @@
-class Move {
-
-int sourceh;
-int sourcec;
-int targeth;
-int targetc;
-
-public:
-// Take sc coins from heap sh and put tc coins to heap th.
-Move(int sh, int sc, int th, int tc);
-
-int getSource() const;
-int getSourceCoins() const;
-int getTarget() const;
-int getTargetCoins() const;
-
-friend ostream & operator << (ostream &out, const Move &move);
-};
-
-Move::Move(int sh, int sc, int th, int tc){
-sourceh = sh;
-sourcec = sc;
-targeth = th;
-targetc = tc;
-}
-
-int Move::getSource() const {
-return sourceh;
-}
-
-int Move::getSourceCoins() const {
-return sourcec;
-}
-
-int Move::getTarget() const {
-return targeth;
-}
-
-int Move::getTargetCoins() const {
-return targetc;
-}
-
-ostream& operator << (ostream &out, const Move &move){
-if (getTargetCoins() > 0)
-out << "takes " << getSourceCoins() << " coins from heap " << getSource() << " and puts " << getTargetCoins() << " coins to heap " << getTarget();
-else
-out << "takes " << getSourceCoins() << " coins from heap " << getSource() << " and puts nothing";
-return out;
-}
-
+#include <stdexcept>  
+#include <iostream>  
+  
+using namespace std;  
+  
+class Move {  
+  
+int sourceh;  
+int sourcec;  
+int targeth;  
+int targetc;  
+  
+public:  
+// Take sc coins from heap sh and put tc coins to heap th.  
+Move(int sh, int sc, int th, int tc);  
+  
+int getSource() const;  
+int getSourceCoins() const;  
+int getTarget() const;  
+int getTargetCoins() const;  
+  
+friend ostream & operator << (ostream &out, const Move &move);  
+};  
+  
+Move::Move(int sh, int sc, int th, int tc){  
+sourceh = sh;  
+sourcec = sc;  
+targeth = th;  
+targetc = tc;  
+}  
+  
+int Move::getSource() const {  
+return sourceh;  
+}  
+  
+int Move::getSourceCoins() const {  
+return sourcec;  
+}  
+  
+int Move::getTarget() const {  
+return targeth;  
+}  
+  
+int Move::getTargetCoins() const {  
+return targetc;  
+}  
+  
+ostream & operator << (ostream &out, const Move &move){  
+if (move.getTargetCoins() > 0)  
+out << "takes " << move.getSourceCoins() << " coins from heap " << move.getSource() << " and puts " << move.getTargetCoins() << " coins to heap " << move.getTarget();  
+else  
+out << "takes " << move.getSourceCoins() << " coins from heap " << move.getSource() << " and puts nothing";  
+return out;  
+}  
+  
